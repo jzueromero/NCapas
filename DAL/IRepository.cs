f3c3a5cc8ad11480bd0cae68634e580efba9e95c
JzueRomero <rj.josueromero@gmail.com>
2020-03-03 16:30:25 -0600

    Creacion de un archivo generico para el patron Repository, esta interfaz podria haberse creado en un proyecto aparte para que cualquiera que le necesitare utilizarlo.

--- DAL/IRepository.cs
+++ DAL/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,22 @@ namespace DAL
 {
     public interface IRepository : IDisposable
     {
+        //Para agregar una entidad a la base de datos
+        TEntity Create<TEntity>(TEntity ToCreate) where TEntity : class;
 
+        //Para eliminar una entidad
+        bool Delete<TEntity>(TEntity toDelete) where TEntity : class;
 
+        // Para Actualizar una entidad
+        bool Update<TEntity>(TEntity toUpdate) where TEntity : class;
+
+        // Para recuperar una entidad en vase a un criterio
+        TEntity Retrieve<TEntity>(Expression<Func<TEntity, bool>> criteria)
+            where TEntity : class;
+
+        //Para recuperar un conjunto de entidades
+        //Que cumplan con un criterio de busqueda
+        List<TEntity> Filter<TEntity>(Expression<Func<TEntity, bool>> criteria)
+            where TEntity : class;
     }
 }
