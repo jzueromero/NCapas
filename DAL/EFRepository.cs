1bb8e42970b0df94f43602eb309cd3c9376f1b30
JzueRomero <rj.josueromero@gmail.com>
2020-03-03 16:59:37 -0600

    Se ha implementado el crud completo generico ademas de la interfaz IDipose

--- DAL/EFRepository.cs
+++ DAL/EFRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,5 +29,60 @@ namespace DAL
             return Result;
         }
 
+        public bool Delete<TEntity>(TEntity toDelete) where TEntity : class
+        {
+            bool Result = false;
+            try
+            {
+                Context.Entry<TEntity>(toDelete).State = EntityState.Deleted;
+                Result = Context.SaveChanges() > 0;
+            }
+            catch { }
+            return Result;
+        }
+
+        public bool Update<TEntity>(TEntity toUpdate) where TEntity : class
+        {
+            bool Result = false;
+            try
+            {
+                Context.Entry<TEntity>(toUpdate).State = EntityState.Modified;
+                Result = Context.SaveChanges() > 0;
+            }
+            catch { }
+            return Result;
+        }
+
+        public TEntity Retrieve<TEntity>(Expression<Func<TEntity, bool>> criteria)
+            where TEntity : class
+        {
+            TEntity Result = null;
+            try
+            {
+                Result = Context.Set<TEntity>().FirstOrDefault(criteria);
+            }
+            catch { }
+            return Result;
+        }
+
+        public List<TEntity> Filter<TEntity>(Expression<Func<TEntity,bool>> criteria)
+            where TEntity : class
+        {
+            List<TEntity> Result = null;
+            try
+            {
+                return Context.Set<TEntity>().Where(criteria).ToList();
+            }
+            catch { }
+            return Result;
+        }
+
+        public void Dispose()
+        {
+            if (Context != null)
+            {
+                Context.Dispose();
+            }
+        }
     }
 }
