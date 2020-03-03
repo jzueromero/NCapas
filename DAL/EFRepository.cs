23a4717036f683474f6ab1698486eaf89df28030
JzueRomero <rj.josueromero@gmail.com>
2020-03-03 16:38:23 -0600

    Una buena practica seria tener por separado el repositorio en un proyecto aparte, mas en la misma capa DAL tendermos la definicion e implementacion del Repository. Esta implemnetacion hara uso de la interfaz IRepository y el acceso a EF 6 con la clase DbContext

---
+++ DAL/EFRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class EFRepository : IRepository
+    {
+        DbContext Context;
+        public EFRepository(DbContext context )
+        {
+            this.Context = context;
+        }
+    }
+}
