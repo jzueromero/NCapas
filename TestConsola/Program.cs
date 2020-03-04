22c25e2f40b795f9cba8a2a12c7921743b646ebe
JzueRomero <rj.josueromero@gmail.com>
2020-03-04 09:55:20 -0600

    Se ha probado todo el funciomaiento de la capa de datos y de entidades. Y funcionan perfectamente.

--- TestConsola/Program.cs
+++ TestConsola/Program.cs
@@ -12,7 +12,11 @@ namespace TestConsola
     {
         static void Main(string[] args)
         {
-            AddCategoryAndProduct();
+            // AddCategoryAndProduct();
+            //AddProduct();
+            //RetriveAndUpdate();
+            //List();
+            SearchAndDelete();
             Console.WriteLine("Presione para continuar");
             Console.ReadKey();
         }
@@ -39,6 +43,72 @@ namespace TestConsola
             Console.WriteLine($"Categoria:{c.CategoryID}, " +
                 $"Producto:{Cereal.ProductID}");
 
+        }
+
+        static void AddProduct()
+        {
+            Products Avena = new Products
+            {
+                CategoryID = 1,
+                UnitsInStock = 100,
+                ProductName = "Avena",
+                UnitPrice = 10
+            };
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                r.Create(Avena);
+            }
+            Console.WriteLine($"Producto: {Avena.ProductID}");
+        }
+
+        static void RetriveAndUpdate()
+        {
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                //buscar el ultimo agregado
+                var P = r.Retrieve<Products>(p => p.ProductID == 2);
+                if (P != null)
+                {
+                    Console.WriteLine(P.ProductName);
+                    P.ProductName = P.ProductName + "***";
+                    r.Update(P);
+                    Console.WriteLine("Nombre modificado");
+                }
+                else
+                {
+                    Console.WriteLine("Producto no encontrado");
+                }
+            }
+        }
+
+        static void List()
+        {
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                var Productos = r.Filter<Products>(p => p.CategoryID == 1);
+                foreach (var p in Productos)
+                {
+                    Console.WriteLine($"{p.ProductName}");
+                }
+            }
+        }
+
+        static void SearchAndDelete()
+        {
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                var P = r.Retrieve<Products>(p => p.ProductID == 2);
+                if (P != null)
+                {
+                    Console.WriteLine(P.ProductName);
+                    r.Delete(P);
+                    Console.WriteLine("Producto Eliminado");
+                }
+                else
+                {
+                    Console.WriteLine("Producto no encontrado");
+                }
+            }
         }
     }
 }
