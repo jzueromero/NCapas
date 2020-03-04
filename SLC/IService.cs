dc4de1143e05de938e35ff94c2763ead9c11bdf4
JzueRomero <rj.josueromero@gmail.com>
2020-03-04 10:48:22 -0600

    Agregamos todas las funciones necesarias para cumplir la necesidades de nuestra logica de negocio

---
+++ SLC/IService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntitiesStandart;
+
+namespace SLC
+{
+    public interface IService
+    {
+        Products CreateProduct(Products newProduct);
+        Products RetrieveProductById(int ID);
+        bool UpdateProduct(Products ProductToUpdate);
+        bool DeleteProduct(int ID);
+        List<Products> FilterProductByCategoryID(int ID);
+        Categories CreateCategory(Categories newCategory);
+    }
+}
