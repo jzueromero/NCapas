d24cbe58e49dcd4907700ecdcf7aea39da032202
JzueRomero <rj.josueromero@gmail.com>
2020-03-04 11:12:59 -0600

    Creamos un controlador, e iniciamos la implementacion de la interfaz de servicio IService

---
+++ Service/Controllers/NWindController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EntitiesStandart;
+using BLL;
+using SLC;
+
+namespace Service.Controllers
+{
+    public class NWindController : ApiController, IService
+    {
+        public EntitiesStandart.Categories CreateCategory(EntitiesStandart.Categories newCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Products CreateProduct(Products newProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProduct(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Products> FilterProductByCategoryID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Products RetrieveProductById(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateProduct(Products ProductToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
