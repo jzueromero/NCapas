0d3af5a87170b518f4d3c3de86e3d9ad24c02c5d
JzueRomero <rj.josueromero@gmail.com>
2020-03-06 16:17:26 -0600

    Agregamos la clase proxy, esta debera agregar las referencias al proyecto SLC y Entities. Ademas de definir cual sera la direccion base del servicio.

---
+++ NWindProxyService/Proxy.cs
@@ -0,0 +1,14 @@
+﻿using SLC;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EntitiesStandart;
+
+namespace NWindProxyService
+{
+    public class Proxy : IService
+    {
+        string BaseAddress = "http://localhost:59167/";
+
+    }
+}
