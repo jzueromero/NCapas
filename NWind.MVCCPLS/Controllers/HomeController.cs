8aaea85761c67935b2e5d77ad308136e7223c72d
JzueRomero <rj.josueromero@gmail.com>
2020-03-07 11:36:17 -0600

    A la accion Index que se creo por defecto en nuestro controlador Home, le agregamos una vista vacia, sin un modelo  y sin usar un layout.

--- NWind.MVCCPLS/Controllers/HomeController.cs
+++ NWind.MVCCPLS/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//referencias
+using NWindProxyService;
+using EntitiesStandart;
 
 namespace NWind.MVCCPLS.Controllers
 {
