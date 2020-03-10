6a1914f7602b7a570fbaabf722cdee7ad987226f
JzueRomero <rj.josueromero@gmail.com>
2020-03-10 09:58:37 -0600

    Creamos el comando para crear un producto

--- NWind.ViewModel/CUD.cs
+++ NWind.ViewModel/CUD.cs
@@ -6,5 +6,26 @@ namespace NWind.ViewModel
 {
     public class CUD : ViewModelBase
     {
+        public CUD()
+        {
+            CreateProductoCommand = new CommandDelegate
+                (
+                    (o) => { return true; },
+                    (o) => 
+                        {
+                            var NewProduct = new EntitiesStandart.Products
+                            {
+                                ProductName = ProductName_BF,
+                                CategoryID = CategoryID_BF,
+                                UnitsInStock = UnitsInStock_BF,
+                                UnitPrice = UnitPrice_BF
+                            };
+                            var Proxy = new NWindProxyService.Proxy();
+                            NewProduct = Proxy.CreateProduct(newProduct);
+                            ProductID = NewProduct.ProductID;
+
+                        }
+                );
+        }
     }
 }
