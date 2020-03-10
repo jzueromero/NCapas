0066785546a2715f369385f6915504b9990471d5
JzueRomero <rj.josueromero@gmail.com>
2020-03-10 08:37:22 -0600

    Con la implementacion de ICommand ahora ya podremos enlazar los botones de la vista con el viewmodel

--- NWind.ViewModel/CommandDelegate.cs
+++ NWind.ViewModel/CommandDelegate.cs
@@ -7,16 +7,39 @@ namespace NWind.ViewModel
 {
     public class CommandDelegate : ICommand
     {
+        Action<object> ExecuteDelegate;
+        Func<object, bool> CanExecuteDelegate;
         public event EventHandler CanExecuteChanged;
+
+        public CommandDelegate(Func<object, bool> canExecuteDelegate, 
+                    Action<object> executeDelegate)
+        {
+            this.CanExecuteDelegate = canExecuteDelegate;
+            this.ExecuteDelegate = executeDelegate;
+
+        }
+
+        public void ChangeCanExecute()
+        {
+            var Handler = CanExecuteChanged;
+            Handler?.Invoke(this, EventArgs.Empty);
+        }
 
         public bool CanExecute(object parameter)
         {
-            throw new NotImplementedException();
+            var Handler = CanExecuteDelegate;
+            bool Result = false;
+            if (Handler != null)
+            {
+                Result = Handler(parameter);
+            }
+            return Result;
         }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            var Handler = ExecuteDelegate;
+            Handler?.Invoke(parameter);
         }
     }
 }
