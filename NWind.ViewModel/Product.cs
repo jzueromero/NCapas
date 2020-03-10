82cbf95d76e535636ece82275bbde2d94d91cdeb
JzueRomero <rj.josueromero@gmail.com>
2020-03-10 08:42:53 -0600

    Agregamos la clase model heredando de ViewModelBase

---
+++ NWind.ViewModel/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EntitiesStandart;
+
+namespace NWind.ViewModel
+{
+    public class Product : ViewModelBase
+    {
+
+    }
+}
